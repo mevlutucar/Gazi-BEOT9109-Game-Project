e3ad081b35819e7d38aa48d5a22b5281ae93f9b4
mevlutucar <ucarcodes@gmail.com>
2026-05-29 12:40:36 +0300

    v0.22 - Musics additions
    Music addition made for Main Menu, Credits Menu and Options Menu scenes.
    Also, game level musics added too.

---
+++ Assets/Scripts/MenuMusicManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMusicManager : MonoBehaviour
+{
+    private static MenuMusicManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "GameMainLevel")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
